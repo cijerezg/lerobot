c4be63cb2becd4dc85fa2ce50917ac3514cfc24d
Jack Vial <vialjack@gmail.com>
2026-04-18 12:35:20 -0400

    add import back

--- src/lerobot/scripts/offline_learner_pi05.py
+++ src/lerobot/scripts/offline_learner_pi05.py
@@ -38,6 +38,8 @@ from accelerate.utils import DistributedDataParallelKwargs
 from termcolor import colored
 from torch import nn
 
+import lerobot.rl.rl_pi05  # noqa: F401  # register PI05RLConfig (pi05_rl policy type)
+
 from lerobot.cameras import opencv  # noqa: F401
 from lerobot.configs import parser
 from lerobot.configs.train import TrainRLServerPipelineConfig
